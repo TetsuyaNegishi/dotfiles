61fe39b79141991ade45b4efb08ce6447f9ac2fd
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2025-08-18 22:22:11 +0900

    feat: add OrbStack command-line tools and completions for Docker, Kubectl, and Orbctl

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,7 @@ end
 function fish_user_key_bindings
     bind \cr peco_select_history
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
